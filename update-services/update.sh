36e2f389d407cb2cbe6697e3f223a513ae2e7632
Sami Cheema <samitcheema@gmail.com>
2020-12-01 11:52:51 -0500

    lecture walkthrough

--- update-services/update.sh
+++ update-services/update.sh
@@ -6,4 +6,4 @@ docker service update --image nginx:1.13.6 web  # change image of web to a previ
 
 docker service update --publish-rm 8088 --publish-add 9090:80 web   # update port of web 
 
-docker service update --f web   # force update to reallocate tasks to lesser used nodes
+docker service update --force web   # force update to reallocate tasks to lesser used nodes
